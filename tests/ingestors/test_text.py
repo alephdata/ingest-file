9e80520b90020699207ebb7d1bd2b884774224da
Stas SUȘCOV <stas@nerd.ro>
2017-03-14 19:54:59 +0000

    Cleanup text ingestor test.

--- tests/ingestors/test_text.py
+++ tests/ingestors/test_text.py
@@ -9,17 +9,6 @@ from ..support import TestCase
 
 class TextIngestorTest(TestCase):
 
-    def test_ingest_forces_utf8(self):
-        fixture_path = self.fixture('non_utf.txt')
-
-        with io.open(fixture_path, encoding='utf-8') as fio:
-            ing = TextIngestor(fio, fixture_path)
-            ing.run()
-
-        self.assertIsNone(ing.body)
-        self.assertEqual(ing.status, TextIngestor.STATUSES.FAILURE)
-        self.assertEqual(ing.state, TextIngestor.STATES.FINISHED)
-
     def test_ingest_on_unicode_file(self):
         fixture_path = self.fixture('utf.txt')
 
@@ -27,7 +16,7 @@ class TextIngestorTest(TestCase):
             ing = TextIngestor(fio, fixture_path)
             ing.run()
 
-        self.assertEqual(ing.body.strip(), u'Îș unî©ođ€.')
+        self.assertEqual(ing.body, u'Îș unî©ođ€.')
         self.assertEqual(ing.status, TextIngestor.STATUSES.SUCCESS)
         self.assertEqual(ing.state, TextIngestor.STATES.FINISHED)
 
@@ -38,9 +27,7 @@ class TextIngestorTest(TestCase):
             ing = TextIngestor(fio, fixture_path)
             ing.run()
 
-        self.assertEqual(ing.body.strip(), u'În latin1.')
-        self.assertEqual(ing.status, TextIngestor.STATUSES.SUCCESS)
-        self.assertEqual(ing.state, TextIngestor.STATES.FINISHED)
+        self.assertEqual(ing.body, u'În latin1.')
 
     @skipUnless(TestCase.EXTRA_FIXTURES, 'No extra fixtures.')
     def test_ingest_extra_fixture(self):
@@ -51,5 +38,3 @@ class TextIngestorTest(TestCase):
             ing.run()
 
         self.assertIsNotNone(ing.body)
-        self.assertEqual(ing.status, TextIngestor.STATUSES.SUCCESS)
-        self.assertEqual(ing.state, TextIngestor.STATES.FINISHED)
