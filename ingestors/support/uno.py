20b0ce1dee6c7aba90cb99686e639005c9a7f41e
Friedrich Lindenberg <friedrich@pudo.org>
2018-02-04 13:38:12 +0100

    Bring down noise level from unoservice a little bit.

--- ingestors/support/uno.py
+++ ingestors/support/uno.py
@@ -29,35 +29,37 @@ class UnoconvSupport(object):
             self._unoconv_client.session = requests.Session()
         return self._unoconv_client.session
 
-    def unoconv_to_pdf(self, file_path, temp_dir):
+    def unoconv_to_pdf(self, file_path, temp_dir, retry=3):
         """Converts an office document to PDF."""
         if not self.is_unoconv_available():
             raise ConfigurationException("UNOSERVICE_URL is missing.")
 
         log.info('Converting [%s] to PDF...', self.result)
         file_name = os.path.basename(file_path)
-        out_path = join_path(temp_dir, '%s.pdf' % file_name)
-        for try_num in range(3):
-            try:
-                with open(file_path, 'rb') as fh:
-                    data = {'format': 'pdf', 'doctype': 'document'}
-                    files = {'file': (file_name, fh, self.UNO_MIME)}
-                    # http://docs.python-requests.org/en/latest/user/advanced/#chunk-encoded-requests
-                    res = self.unoconv_client.post(self.get_unoconv_url(),
-                                                   data=data,
-                                                   files=files,
-                                                   timeout=300.0,
-                                                   stream=True)
-                length = 0
-                with open(out_path, 'w') as fh:
-                    for chunk in res.iter_content(chunk_size=None):
-                        length += len(chunk)
-                        fh.write(chunk)
+        out_path = join_path(temp_dir, '%s.pdf' % file_name)    
+        try:
+            with open(file_path, 'rb') as fh:
+                data = {'format': 'pdf', 'doctype': 'document'}
+                files = {'file': (file_name, fh, self.UNO_MIME)}
+                res = self.unoconv_client.post(self.get_unoconv_url(),
+                                               data=data,
+                                               files=files,
+                                               timeout=300.0,
+                                               stream=True)
+            length = 0
+            with open(out_path, 'w') as fh:
+                for chunk in res.iter_content(chunk_size=None):
+                    length += len(chunk)
+                    fh.write(chunk)
 
-                if length == 0:
-                    raise ProcessingException("Could not convert to PDF.")
-                return out_path
-            except RequestException:
-                log.exception('Error contacting unoservice.')
-                time.sleep(3 ** try_num)
-        raise ProcessingException("Could not convert to PDF.")
+            if length == 0:
+                raise ProcessingException("Could not convert to PDF.")
+            return out_path
+        except RequestException:
+            if retry > 0:
+                log.warning("unoservice not available, retry %s...", retry)
+                time.sleep(3)
+                return self.unoconv_to_pdf(file_path, temp_dir,
+                                           retry=retry - 1)
+            log.exception('Error contacting unoservice.')
+            raise ProcessingException("Could not convert to PDF.")
