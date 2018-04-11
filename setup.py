063309fb85f143c86d68edcbb4e9f40c1247a3e9
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-11 20:38:10 +0200

    Bump version: 0.7.15 → 0.7.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ingestors',
-    version='0.7.15',
+    version='0.7.16',
     description="Ingestors extract useful information in a structured standard format.",  # noqa
     author="Organized Crime and Corruption Reporting Project",
     author_email='data@occrp.org',
