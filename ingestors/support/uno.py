4d0cdec87d67b2d9f6f88d36bf2407d4302069f5
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-25 08:18:17 +0200

    Simplify unoservice a bit.

--- ingestors/support/uno.py
+++ ingestors/support/uno.py
@@ -2,7 +2,6 @@ import os
 import time
 import logging
 import requests
-import threading
 from celestial import DEFAULT
 from requests.exceptions import RequestException
 
@@ -21,15 +20,7 @@ class UnoconvSupport(object):
     def is_unoconv_available(self):
         return self.get_unoconv_url() is not None
 
-    @property
-    def unoconv(self):
-        if not hasattr(self, '_unoconv_client'):
-            self._unoconv_client = threading.local()
-        if not hasattr(self._unoconv_client, 'session'):
-            self._unoconv_client.session = requests.Session()
-        return self._unoconv_client.session
-
-    def unoconv_to_pdf(self, file_path, retry=5, timeout=300):
+    def unoconv_to_pdf(self, file_path, retry=5):
         """Converts an office document to PDF."""
         if not self.is_unoconv_available():
             raise ConfigurationException("UNOSERVICE_URL is missing.")
@@ -43,26 +34,18 @@ class UnoconvSupport(object):
         while attempt <= retry:
             fh = open(file_path, 'rb')
             try:
-                params = {'timeout': timeout}
                 files = {'file': (file_name, fh, mime_type)}
-                res = self.unoconv.post(self.get_unoconv_url(),
-                                        files=files,
-                                        params=params,
-                                        timeout=(5, timeout),
-                                        stream=True)
-            except RequestException:
-                log.exception("unoservice connection error")
-                time.sleep(2 * attempt)
+                res = requests.post(self.get_unoconv_url(),
+                                    files=files,
+                                    timeout=(5, 305),
+                                    stream=True)
+            except RequestException as exc:
+                log.warning("Conversion failed: %s", exc)
+                time.sleep(2 ** attempt)
                 attempt += 1
                 continue
             finally:
                 fh.close()
-
-            # check for busy signal
-            if res.status_code == 503:
-                # wait for TTL on RR DNS to expire.
-                time.sleep(2 * attempt)
-                continue
 
             if res.status_code == 400:
                 raise ProcessingException(res.text)
