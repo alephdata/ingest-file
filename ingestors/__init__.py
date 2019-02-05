b25dbfa7d1f4eacdc13d73ccb6b14044a3123af1
Friedrich Lindenberg <friedrich@pudo.org>
2019-02-05 09:05:57 +0100

    Bump version: 0.11.3 → 0.12.0

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -4,7 +4,7 @@ import logging
 from ingestors.manager import Manager
 from ingestors.result import Result  # noqa
 
-__version__ = '0.11.3'
+__version__ = '0.12.0'
 
 logging.getLogger('chardet').setLevel(logging.INFO)
 logging.getLogger('flanker').setLevel(logging.INFO)
