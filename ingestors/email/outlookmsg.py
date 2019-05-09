25d385e97226ad187d63573bf7184cb0ea91881a
Tarashish Mishra <sunu0000@gmail.com>
2019-05-09 14:11:11 +0530

    don't redefine

--- ingestors/email/outlookmsg.py
+++ ingestors/email/outlookmsg.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import logging
 from olefile import isOleFile
 from email.parser import Parser
+from followthemoney import model
 
 from ingestors.ingestor import Ingestor
 from ingestors.support.email import EmailSupport
@@ -28,13 +29,13 @@ class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
         headers = message.getField('007D')
         if headers is not None:
             try:
-                message = Parser().parsestr(headers, headersonly=True)
-                self.extract_headers_metadata(entity, message.items())
+                msg = Parser().parsestr(headers, headersonly=True)
+                self.extract_headers_metadata(entity, msg.items())
                 return
             except Exception:
                 log.warning("Cannot parse headers: %s" % headers)
 
-        self.result.headers = safe_dict({
+        headers = safe_dict({
             'Subject': message.getField('0037'),
             'BCC': message.getField('0E02'),
             'CC': message.getField('0E03'),
@@ -42,8 +43,10 @@ class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
             'From': message.getField('1046'),
             'Message-ID': message.getField('1035'),
         })
+        self.extract_headers_metadata(entity, headers)
 
     def ingest(self, file_path, entity):
+        entity.schema = model.get('Email')
         message = Message(file_path)
         self._parse_headers(entity, message)
         entity.add('bodyText', message.getField('1000'))
