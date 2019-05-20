005b3655a71d1d66044965298fab3087a914bb43
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-20 15:39:03 +0200

    Try to fix up doc service usage

--- ingestors/task_runner.py
+++ ingestors/task_runner.py
@@ -14,19 +14,33 @@ class TaskRunner(object):
     """A long running task runner that uses Redis as a task queue"""
 
     @classmethod
-    def process(cls):
-        conn = get_redis()
-        while True:
-            task = Queue.get_operation_task(conn, Queue.OP_INGEST)
-            queue, payload, context = task
-            if payload is None:
-                return
+    def handle_task(cls, queue, payload, context):
+        try:
             manager = Manager(queue, context)
             entity = model.get_proxy(payload)
             log.debug("Received: %r", entity)
             file_path = manager.get_filepath(entity)
             manager.ingest(file_path, entity)
             manager.close()
+        finally:
+            queue.task_done()
+            status = queue.progress.get()
+            if status.get('pending') < 1:
+                index_queue = Queue(queue.conn,
+                                    Queue.OP_INDEX,
+                                    queue.dataset,
+                                    priority=queue.priority)
+                index_queue.queue_task({}, {})
+
+    @classmethod
+    def process(cls):
+        conn = get_redis()
+        while True:
+            task = Queue.get_operation_task(conn, Queue.OP_INGEST)
+            queue, payload, context = task
+            if queue is None:
+                continue
+            cls.handle_task(queue, payload, context)
 
     @classmethod
     def run(cls):
