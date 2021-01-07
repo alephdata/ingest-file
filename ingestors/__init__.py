bec304a3c21f4a592297cf6a1c55537ce1ff1e61
Friedrich Lindenberg <friedrich@pudo.org>
2021-01-07 20:39:39 +0100

    Bump version: 3.11.0 → 3.11.1

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -1,7 +1,7 @@
 """Provides a set of ingestors based on different file types."""
 import logging
 
-__version__ = "3.11.0"
+__version__ = "3.11.1"
 
 logging.getLogger("chardet").setLevel(logging.INFO)
 logging.getLogger("PIL").setLevel(logging.INFO)
