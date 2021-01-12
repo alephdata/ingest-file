48594a650f1ca8348cc66261bab8c2d3ae2b9113
Friedrich Lindenberg <friedrich@pudo.org>
2021-01-12 09:51:53 +0100

    Bump version: 3.11.1 → 3.11.2

--- convert/setup.py
+++ convert/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="convert",
-    version="3.11.1",
+    version="3.11.2",
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
