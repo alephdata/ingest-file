c0265a29521414382908ab92409923987bf1c489
Friedrich Lindenberg <friedrich@pudo.org>
2021-04-21 14:02:05 +0200

    Bump version: 3.13.2 → 3.14.0

--- convert/setup.py
+++ convert/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="convert",
-    version="3.13.2",
+    version="3.14.0",
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -1,7 +1,7 @@
 """Provides a set of ingestors based on different file types."""
 import logging
 
-__version__ = "3.13.2"
+__version__ = "3.14.0"
 
 logging.getLogger("chardet").setLevel(logging.INFO)
 logging.getLogger("PIL").setLevel(logging.INFO)
