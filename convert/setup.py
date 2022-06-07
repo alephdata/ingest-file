1ac833f5a5b2dac40b7f5bdde76a6bde662b3997
Steve Haffenden <steve.haffenden@occrp.org>
2022-06-07 14:36:13 +0200

    Bump version: 3.16.2 → 3.16.3

--- convert/setup.py
+++ convert/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="convert",
-    version="3.16.2",
+    version="3.16.3",
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
