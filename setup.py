bc7d625b0e4cd560e2ac2db3f8f690728c0d4ca1
Christian Stefanescu <chris@stchris.net>
2022-11-03 11:30:59 +0100

    Bump version: 3.16.4 → 3.17.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ingest",
-    version="3.16.4",
+    version="3.17.0",
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=["tests"]),
     package_dir={"ingestors": "ingestors"},
