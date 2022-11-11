f09e8b56da5a1288f8a4df5255b8e5626629e925
Christian Stefanescu <chris@stchris.net>
2022-11-11 17:34:13 +0100

    WIP finish refactoring pdf support class

--- ingestors/documents/pdf.py
+++ ingestors/documents/pdf.py
@@ -4,10 +4,6 @@ from ingestors.ingestor import Ingestor
 from ingestors.support.pdf import PDFSupport, PdfModel
 from ingestors.exc import ProcessingException
 
-from followthemoney import model
-
-log = logging.getLogger(__name__)
-
 
 class PDFIngestor(Ingestor, PDFSupport):
     """PDF file ingestor class.
@@ -21,46 +17,10 @@ class PDFIngestor(Ingestor, PDFSupport):
     EXTENSIONS = ["pdf"]
     SCORE = 6
 
-    def extract_xmp_metadata(self, pdf, entity):
-        try:
-            xmp = pdf.xmp_metadata
-            if xmp is None:
-                return
-            entity.add("messageId", xmp["xmpmm"].get("documentid"))
-            entity.add("title", xmp["dc"].get("title"))
-            entity.add("generator", xmp["pdf"].get("producer"))
-            entity.add("language", xmp["dc"].get("language"))
-            entity.add("authoredAt", xmp["xmp"].get("createdate"))
-            entity.add("modifiedAt", xmp["xmp"].get("modifydate"))
-        except Exception as ex:
-            log.warning("Error reading XMP: %r", ex)
-
-    def extract_metadata(self, pdf, entity):
-        meta = pdf.metadata
-        if meta is not None:
-            entity.add("title", meta.get("title"))
-            entity.add("author", meta.get("author"))
-            entity.add("generator", meta.get("creator"))
-            entity.add("generator", meta.get("producer"))
-            entity.add("keywords", meta.get("subject"))
-
     def ingest(self, file_path, entity):
         """Ingestor implementation."""
         try:
-            pdf_model: PdfModel = self.parse(file_path)
-            self.extract_metadata(pdf_model, entity)
-            self.extract_xmp_metadata(pdf_model, entity)
-
-            entity.schema = model.get("Pages")
-            for page_model in pdf_model.pages:
-                page_entity = self.manager.make_entity("Page")
-                page_entity.make_id(entity.id, page_model.number)
-                page_entity.set("document", entity)
-                page_entity.set("index", page_model.number)
-                page_entity.add("bodyText", page_model.text)
-                self.manager.apply_context(page_entity, entity)
-                self.manager.emit_entity(page_entity)
-                self.manager.emit_text_fragment(entity, page_model.text, entity.id)
+            self.parse_and_ingest(file_path, entity, self.manager)
         except Exception as ex:
             raise ProcessingException("Could not extract PDF file: %r" % ex) from ex
 
--- ingestors/support/pdf.py
+++ ingestors/support/pdf.py
@@ -13,20 +13,28 @@ from pdfminer.pdfparser import PDFParser
 
 from normality import collapse_spaces  # noqa
 
+from followthemoney import model
 from ingestors.support.ocr import OCRSupport
 from ingestors.support.convert import DocumentConvertSupport
 
+# silence some shouty debug output from pdfminer
 logging.getLogger("pdfminer").setLevel(logging.WARNING)
+
+log = logging.getLogger(__name__)
 
 
 @dataclass
 class PdfPageModel:
+    """Represents data extracted from a page in a PDF"""
+
     number: int
     text: str
 
 
 @dataclass
 class PdfModel:
+    """Represents data extracted from a PDF"""
+
     metadata: Dict[str, str]
     xmp_metadata: Dict[str, str]
     pages: List[PdfPageModel]
@@ -35,20 +43,61 @@ class PdfModel:
 class PDFSupport(DocumentConvertSupport, OCRSupport):
     """Provides helpers for PDF file context extraction."""
 
+    def extract_xmp_metadata(self, pdf, entity):
+        try:
+            xmp = pdf.xmp_metadata
+            if xmp is None:
+                return
+            entity.add("messageId", xmp["xmpmm"].get("documentid"))
+            entity.add("title", xmp["dc"].get("title"))
+            entity.add("generator", xmp["pdf"].get("producer"))
+            entity.add("language", xmp["dc"].get("language"))
+            entity.add("authoredAt", xmp["xmp"].get("createdate"))
+            entity.add("modifiedAt", xmp["xmp"].get("modifydate"))
+        except Exception as ex:
+            log.warning("Error reading XMP: %r", ex)
+
+    def extract_metadata(self, pdf, entity):
+        meta = pdf.metadata
+        if meta is not None:
+            entity.add("title", meta.get("title"))
+            entity.add("author", meta.get("author"))
+            entity.add("generator", meta.get("creator"))
+            entity.add("generator", meta.get("producer"))
+            entity.add("keywords", meta.get("subject"))
+
+    def extract_pages(self, pdf_model, entity, manager):
+        entity.schema = model.get("Pages")
+        for page_model in pdf_model.pages:
+            page_entity = self.manager.make_entity("Page")
+            page_entity.make_id(entity.id, page_model.number)
+            page_entity.set("document", entity)
+            page_entity.set("index", page_model.number)
+            page_entity.add("bodyText", page_model.text)
+            manager.apply_context(page_entity, entity)
+            manager.emit_entity(page_entity)
+            manager.emit_text_fragment(entity, page_model.text, entity.id)
+
     def parse(self, file_path: str) -> PdfModel:
-        model = PdfModel(metadata=None, xmp_metadata=None, pages=[])
+        """Takes a file_path to a pdf and returns a `PdfModel`"""
+        pdf_model = PdfModel(metadata=None, xmp_metadata=None, pages=[])
         with open(file_path, "rb") as pdf_file:
             parser = PDFParser(pdf_file)
             pdf_doc = PDFDocument(parser)
             for page_number, page in enumerate(PDFPage.create_pages(pdf_doc), 1):
-                model.pages.append(self.pdf_extract_page(page, page_number))
-        return model
+                pdf_model.pages.append(self.pdf_extract_page(page, page_number))
+        return pdf_model
 
-    def pdf_alternative_extract(self, entity, pdf_path):
+    def parse_and_ingest(self, file_path, entity, manager):
+        pdf_model: PdfModel = self.parse(file_path)
+        self.extract_metadata(pdf_model, entity)
+        self.extract_xmp_metadata(pdf_model, entity)
+        self.extract_pages(pdf_model, entity, manager)
+
+    def pdf_alternative_extract(self, entity, pdf_path, manager):
         checksum = self.manager.store(pdf_path)
         entity.set("pdfHash", checksum)
-        pdf = PDFDocument(bytes(pdf_path))
-        self.pdf_extract(entity, pdf)
+        self.parse_and_ingest(pdf_path, entity, manager)
 
     def pdf_extract_page(self, page: PDFPage, page_number: int) -> PdfPageModel:
         """Extract the contents of a single PDF page, using OCR if need be."""
