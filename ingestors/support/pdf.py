d2c0494bba4e73408e1937775f98b8dde9eb4b37
Christian Stefanescu <christian.stefanescu@occrp.org>
2022-11-21 10:05:58 +0100

    WIP pdfreader (RIP so slow)

--- ingestors/support/pdf.py
+++ ingestors/support/pdf.py
@@ -6,12 +6,8 @@ import os
 from typing import Dict, List
 import uuid
 
-from pdfminer.converter import TextConverter, PDFPageAggregator
-from pdfminer.layout import LAParams, LTImage, LTFigure
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfparser import PDFParser
+from pdfreader import SimplePDFViewer
+from pdfreader.viewer import SimpleCanvas
 
 from normality import collapse_spaces  # noqa
 
@@ -84,9 +80,8 @@ class PDFSupport(DocumentConvertSupport, OCRSupport):
         """Takes a file_path to a pdf and returns a `PdfModel`"""
         pdf_model = PdfModel(metadata=None, xmp_metadata=None, pages=[])
         with open(file_path, "rb") as pdf_file:
-            parser = PDFParser(pdf_file)
-            pdf_doc = PDFDocument(parser)
-            for page_number, page in enumerate(PDFPage.create_pages(pdf_doc), 1):
+            viewer = SimplePDFViewer(pdf_file)
+            for page_number, page in enumerate(viewer, 1):
                 pdf_model.pages.append(self.pdf_extract_page(page, page_number))
         return pdf_model
 
@@ -101,81 +96,14 @@ class PDFSupport(DocumentConvertSupport, OCRSupport):
         entity.set("pdfHash", checksum)
         self.parse_and_ingest(pdf_path, entity, manager)
 
-    def _write_file(self, folder, filename, filedata, flags="w"):
-        """Write the file data to the folder and filename combination
-        (flags: 'w' for write text, 'wb' for write binary, use 'a' instead of 'w' for append)"""
-        result = False
-        if os.path.isdir(folder):
-            try:
-                file_obj = open(os.path.join(folder, filename), flags)
-                file_obj.write(filedata)
-                file_obj.close()
-                result = True
-            except IOError:
-                pass
-        return result
-
-    def _determine_image_type(self, stream_first_4_bytes):
-        """Find out the image file type based on the magic number comparison of the first 4 (or 2) bytes"""
-        file_type = None
-        bytes_as_hex = b2a_hex(stream_first_4_bytes)
-        print(f"magic bytes {bytes_as_hex}")
-        if bytes_as_hex.startswith(b"ffd8"):
-            file_type = ".jpeg"
-        elif bytes_as_hex == "89504e47":
-            file_type = ".png"
-        elif bytes_as_hex == "47494638":
-            file_type = ".gif"
-        elif bytes_as_hex.startswith(b"424d"):
-            file_type = ".bmp"
-        return file_type
-
-    def _save_image(self, lt_image, page_number, images_folder):
-        """Try to save the image data from this LTImage object, and return the file name, if successful"""
-        result = None
-        if lt_image.stream:
-            file_stream = lt_image.stream.get_rawdata()
-            if file_stream:
-                first_bytes = file_stream[0:4]
-                file_ext = self._determine_image_type(first_bytes) or "xxx"
-                if file_ext:
-                    file_name = "".join(
-                        [str(page_number), "_", lt_image.name, file_ext]
-                    )
-                    if self._write_file(
-                        images_folder, file_name, file_stream, flags="wb"
-                    ):
-                        result = file_name
-                else:
-                    log.debug(f"Unrecognized image starting with {first_bytes}")
-        return result
-
-    def _parse_images(self, layout, page_number, folder):
-        for obj in layout:
-            if isinstance(obj, LTImage):
-                saved_file = self._save_image(obj, page_number, folder)
-                print(f"Found {folder}/{saved_file}")
-            elif isinstance(obj, LTFigure):
-                self._parse_images(obj, page_number, folder)
-
-    def pdf_extract_page(self, page: PDFPage, page_number: int) -> PdfPageModel:
+    def pdf_extract_page(self, page: SimpleCanvas, page_number: int) -> PdfPageModel:
         """Extract the contents of a single PDF page, using OCR if need be."""
         page_model = PdfPageModel(number=page_number, text="")
-        buf = StringIO()
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, buf, laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        interpreter.process_page(page)
+        texts = page.strings
+        # print(f"Also found {len(page.canvas.inline_images)} images")
 
-        # images
-        # temp_dir = self.make_empty_directory()
-        temp_dir = "/tests"
-        rsrcmgr = PDFResourceManager()
-        device = PDFPageAggregator(rsrcmgr)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        interpreter.process_page(page)
-        self._parse_images(device.get_result(), page_number, temp_dir)
-        texts = buf.getvalue()
+        # temp_dir = "/tests"
+        # self._parse_images(device.get_result(), page_number, temp_dir)
         # image_path = temp_dir.joinpath(str(uuid.uuid4()))
         # page.extract_images(path=bytes(image_path), prefix=b"img")
         # languages = self.manager.context.get("languages")
