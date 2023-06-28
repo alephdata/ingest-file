aa35db7186b2152aaf45f09bca74d4b7da0fcdd5
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-06-28 19:40:39 +0200

    Bump version: 3.19.0 → 3.19.1-rc1

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -1,7 +1,7 @@
 """Provides a set of ingestors based on different file types."""
 import logging
 
-__version__ = "3.19.0"
+__version__ = "3.19.1-rc1"
 
 logging.getLogger("chardet").setLevel(logging.INFO)
 logging.getLogger("PIL").setLevel(logging.INFO)
