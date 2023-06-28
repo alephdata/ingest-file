75418c675c780401b59a2a50fbc4abbb0c6eccf2
Alex Stefanescu <alex.stefanescu@protonmail.com>
2023-06-28 11:47:47 +0300

    Paint it black

--- ingestors/manager.py
+++ ingestors/manager.py
@@ -144,7 +144,7 @@ class Manager(object):
         now_string = now.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
         entity.set("processingStatus", self.STATUS_FAILURE)
-        entity.set("processingAgent", get_distribution('ingest').version)
+        entity.set("processingAgent", get_distribution("ingest").version)
         entity.set("processedAt", now_string)
 
         try:
