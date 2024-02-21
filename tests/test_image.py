f7a2322e6e92c3044b4098acdf4a7bd2255f9a3a
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-02-21 12:13:50 +0100

    Revert "Only run one test and print the tesseract ocr version"
    This reverts commit 24fe4fe2a74f7b5f02e59af657a6d38775935a53.

--- tests/test_image.py
+++ tests/test_image.py
@@ -17,15 +17,7 @@ class ImageIngestorTest(TestCase):
     def test_ingest_on_jpeg(self):
         fixture_path, entity = self.fixture("jpegtest.jpg")
         self.manager.ingest(fixture_path, entity)
-        import tesserocr
-
-        try:
-            self.assertIn("Debian", entity.first("bodyText"))
-        except Exception:
-            print(
-                f"tesserocr version: {tesserocr.tesseract_version()}",
-            )
-            self.assertTrue(False)
+        self.assertIn("Debian", entity.first("bodyText"))
         self.assertEqual(entity.first("mimeType"), "image/jpeg")
 
         self.assertEqual(entity.first("processingStatus"), self.manager.STATUS_SUCCESS)
