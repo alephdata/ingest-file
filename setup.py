94ad95f2417f296774329299a53eca240c664a44
Alex Stefanescu <alex.stefanescu@protonmail.com>
2024-03-21 13:46:46 +0100

    Bump version: 4.0.0-rc5 → 4.0.0-rc6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ingest",
-    version="4.0.0-rc5",
+    version="4.0.0-rc6",
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=["tests"]),
     package_dir={"ingestors": "ingestors"},
