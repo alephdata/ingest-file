423fe7d130f0f39c759356d7898337a4b7b20b03
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-07-08 16:43:36 +0200

    Bump version: 4.0.0-rc24 → 4.0.0-rc25

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-__version__ = "4.0.0-rc24"
+__version__ = "4.0.0-rc25"
 
 logging.getLogger("chardet").setLevel(logging.INFO)
 logging.getLogger("PIL").setLevel(logging.INFO)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ingest",
-    version="4.0.0-rc24",
+    version="4.0.0-rc25",
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=["tests"]),
     package_dir={"ingestors": "ingestors"},
