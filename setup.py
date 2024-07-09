8593fb2ebdda69125a1a1807a9c357104f01ef16
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-07-09 11:25:52 +0200

    Bump version: 4.0.0-rc25 → 4.0.0-rc26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ingest",
-    version="4.0.0-rc25",
+    version="4.0.0-rc26",
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=["tests"]),
     package_dir={"ingestors": "ingestors"},
